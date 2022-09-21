130020a6f1853275996430ee9e1e35ce8b817df5
Joe Shambrook <jshambrook@and.digital>
2022-09-21 09:21:22 +0100

    Remove Partytown until I need it again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), svelte(), partytown()],
+  integrations: [preact(), svelte()],
   site: "https://www.josephshambrook.dev",
 });
