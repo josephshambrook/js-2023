2c6b43392ecf9a763b7849a87514bae5422cc093
Joseph Shambrook <751062+josephshambrook@users.noreply.github.com>
2022-09-21 11:56:54 +0100

    Headers (#3)
    * One more crack at headers
    
    * Move the headers file to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), svelte()],
+  integrations: [preact(), svelte(), partytown()],
   site: "https://www.josephshambrook.dev",
 });
