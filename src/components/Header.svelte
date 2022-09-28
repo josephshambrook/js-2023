42e037b5281f9b27af52a5efc908c1397e952677
Joe Shambrook <jshambrook@and.digital>
2022-09-28 17:20:52 +0100

    Hacked together an alt version

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,6 +8,7 @@
 
   // assets
   import logo from "/assets/logos/svg/logo-on-dark.svg";
+  import logoAlt from "/assets/logos/svg/logo-on-white-4.svg";
   import github from "/assets/icons/github.svg";
   import twitter from "/assets/icons/twitter.svg";
 
@@ -16,36 +17,52 @@
 
   // props
   export let isTransparent = false;
+  export let isWhite = false;
 </script>
 
 <header
   class={cns(headerStyles.header, {
     [headerStyles["is-transparent"]]: isTransparent,
+    [headerStyles["is-white"]]: isWhite,
   })}
 >
   <div class={cns(headerStyles.wrapper, layoutStyles.container)}>
     <a class={headerStyles["sr-only"]} href="#maincontent" target="_self"
       >Skip to main content</a
     >
+
     <a class={headerStyles["logo-link"]} href="/">
-      <img
-        src={logo}
-        class={headerStyles["logo-img"]}
-        alt="Joseph Shambrook logo"
-      />
+      {#if isWhite}
+        <img
+          src={logoAlt}
+          class={headerStyles["logo-img"]}
+          alt="Joseph Shambrook logo"
+        />
+      {:else}
+        <img
+          src={logo}
+          class={headerStyles["logo-img"]}
+          alt="Joseph Shambrook logo"
+        />
+      {/if}
     </a>
 
     <nav class={headerStyles.nav}>
       <div class={headerStyles["links-wrap"]}>
         <a href="/posts">Posts</a>
+
         <!-- <a href="#">Snippets</a> -->
+
         <a href="/bio">Bio</a>
+
         <a href={SOCIAL_LINKS.github}>
           <img src={github} alt="GitHub" />
         </a>
+
         <!-- <a href={SOCIAL_LINKS.linkedin}>
           <img src={linkedin} alt="LinkedIn" />
         </a> -->
+
         <a href={SOCIAL_LINKS.twitter}>
           <img src={twitter} alt="Twitter" />
         </a>
