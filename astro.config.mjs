55783611fa4a22c6afbfe3f38cc8b88fdc348047
Joe Shambrook <jshambrook@and.digital>
2022-10-04 09:45:52 +0100

    Remark and Rehype plugins, and enable heading links in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,41 @@
 import { defineConfig } from "astro/config";
+
+// integrations
 import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-// https://astro.build/config
+// these plugins come as default with Astro, but cause a race condition
+// when combined with other plugins, as the defaults are run after my
+// own custom plugins rather than before, and I kind of need them to
+// run before! Therefore, taken the manual step of taking the plugins and
+// installing them myself
+import remarkGfm from "remark-gfm";
+import smartypants from "remark-smartypants";
+
+// custom remark or rehype plugins
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 export default defineConfig({
-  integrations: [preact(), svelte(), react()],
   site: "https://www.josephshambrook.dev",
+  integrations: [preact(), svelte(), react()],
+  markdown: {
+    remarkPlugins: [remarkGfm, () => smartypants({ quotes: false })],
+    rehypePlugins: [
+      rehypeSlug,
+      () =>
+        rehypeAutolinkHeadings({
+          // this is a "hast" element, but I don't want to install hast, so
+          // here is what would be output
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {},
+            children: [{ type: "text", value: "#" }],
+          },
+        }),
+    ],
+    extendDefaultPlugins: false,
+  },
 });
