48239265275dd0cecad77c8252e89d7183d89ccb
Joe Shambrook <jshambrook@and.digital>
2023-01-03 16:30:38 +0000

    feat: :lipstick: create InfoBox component

---
+++ src/components/InfoBox.svelte
@@ -0,0 +1,16 @@
+<script type="ts">
+  import InfoBoxStyles from "@styles/components/InfoBox.module.scss";
+</script>
+
+<section class={InfoBoxStyles.infobox}>
+  {#if $$slots.icon}
+    <!--
+      The icon slot is optional, but we want to wrap the slot
+      in a span to be styled
+    -->
+    <span class={InfoBoxStyles.icon}><slot name="icon" /></span>
+  {/if}
+
+  <slot name="title" />
+  <slot />
+</section>
