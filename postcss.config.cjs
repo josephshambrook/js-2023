ad4a8af39adbc2c4fcce0937bc01fd742384621f
Joseph Shambrook <josephshambrook@googlemail.com>
2023-09-27 08:41:17 +0100

    feat: :lipstick: working media breakpoints from postcss and fixes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,14 @@
-module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("autoprefixer"),
-    require("postcss-custom-media"),
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    "postcss-import": {},
+    autoprefixer: {},
+    "@csstools/postcss-global-data": {
+      files: ["./src/styles2/media.css"],
+    },
+    "postcss-custom-media": {},
+    "postcss-nesting": {},
+  },
 };
+
+module.exports = config;
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,8 +9,8 @@
   import { SOCIAL_LINKS } from "src/constants";
 
   // styles
-  import layoutStyles from "@styles/layouts/layout.module.scss";
-  import footerStyles from "@styles/components/Footer.module.scss";
+  // import layoutStyles from "@styles/layouts/layout.module.scss";
+  // import footerStyles from "@styles/components/Footer.module.scss";
 
   // js
   const sections: FooterSection[] = [
@@ -43,33 +43,32 @@
   ];
 </script>
 
-<footer class={footerStyles.footer}>
-  <div class={cns(layoutStyles.container, layoutStyles["grid-auto"])}>
-    <section class={footerStyles["section-intro"]}>
+<footer class="footer">
+  <div class="container grid-auto">
+    <section class="section-intro">
       <a href="/">
         <img
           src="/assets/logos/svg/logo-on-dark.svg"
-          class={footerStyles.logo}
+          class="logo"
           alt="Joseph Shambrook logo"
         />
       </a>
-      <p class={footerStyles.intro}>
+      <p class="intro">
         A front-end developer, living in Edinburgh, UK.
       </p>
     </section>
     {#each sections as section}
-      <section class={footerStyles["section"]}>
-        <h2 class={footerStyles["section-heading"]}>{section.heading}</h2>
-        <ul class={footerStyles["section-list"]}>
+      <section class="section">
+        <h2 class="section-heading">{section.heading}</h2>
+        <ul class="section-list">
           {#each section.links as link}
             <li>
               <a
                 href={link.href}
-                class={footerStyles["section-link"]}
+                class="section-link"
                 target={link.external ? "_blank" : null}
                 rel={link.external ? "noopener noreferrer" : null}
-                >{link.text}</a
-              >
+                >{link.text}</a>
             </li>
           {/each}
         </ul>
@@ -77,3 +76,52 @@
     {/each}
   </div>
 </footer>
+
+<style lang="postcss">
+.footer {
+  background-color: var(--color-dark-blue);
+  color: var(--color-white);
+  font-family: var(--font-heading);
+  margin-block-start: 2rem;
+  padding-block-end: 5rem;
+  padding-block-start: 2.58rem;
+}
+
+.logo {
+  max-width: 4rem;
+  width: 100%;
+}
+
+.intro {
+  font-size: var(--text-small);
+}
+
+.section-intro {
+  grid-column: 1 / -1;
+
+  @media (--bp-lg) {
+    grid-column: initial;
+  }
+}
+
+.section-heading {
+  color: var(--color-yellow);
+  font-size: var(--heading-6);
+  margin-block-end: 1.3rem;
+  margin-block-start: 0;
+  text-transform: uppercase;
+}
+
+.section-list {
+  list-style-type: none;
+  padding-inline-start: 0;
+}
+
+.section-link {
+  color: var(--color-white);
+  font-size: var(--text-small);
+  font-weight: normal;
+  text-decoration: none;
+}
+
+</style>
